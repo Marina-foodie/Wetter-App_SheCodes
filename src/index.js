acf6fc6ce467820626f5c1474b06648b6183a66f
Marina-foodie <marina.ortner@gmail.com>
2025-05-10 20:13:08 +0200

    API integration

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ searchFormElement.addEventListener("submit", searchSubmit);
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#searchbar");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
+
+function searchCity(city) {
+  let apiKey = "cf40cbba3b587f08e75d7ob82t7ad6ff";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#weather-temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+
+searchCity("St.Gallen");
